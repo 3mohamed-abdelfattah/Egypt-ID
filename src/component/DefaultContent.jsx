dd57704938edb2e98fd94a1a61c4c50a07ea3db8
MohamedMohamed <m01126165016@icloud.com>
2024-08-24 07:04:44 +0300

    Done !!

--- src/component/DefaultContent.jsx
+++ src/component/DefaultContent.jsx
@@ -3,8 +3,8 @@ import Photo from '../assets/img/egypt.png'
 export default function DefaultContent() {
     return (
         <div>
-            <p style={{ fontWeight: '500', fontSize: '18px', lineHeight: '48.08px', color: ' #858585' }}>اكتب أي رقم لكي تظهر لك بعض البيانات عنه</p>
-            <img style={{ width: '30vw' }} src={Photo} />
+            <div style={{ fontWeight: '500', fontSize: '1em', lineHeight: '48.08px', color: ' #858585' }}>اكتب أي رقم لكي تظهر لك بعض البيانات عنه</div>
+            <img style={{ width: '10vw' }} src={Photo} alt='Egypt Icon' />
         </div>
     )
 }
